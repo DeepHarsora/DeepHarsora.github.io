de16f52da67781b23d3cc569c4ae49f8a432f546
DeepHarsora <45792538+DeepHarsora@users.noreply.github.com>
2019-05-03 21:28:32 +0530

    Add files via upload

---
+++ KuldeepProj/hack.js
@@ -0,0 +1,20 @@
+  button.addEventListener('click', async() => {
+    let device
+    const VENDOR_ID = 0x8564
+    
+    try {
+      device = await navigator.usb.requestDevice({
+        filters: [{
+          vendorId: VENDOR_ID
+        }]
+      })
+
+      console.log('open')
+      await device.open()
+      console.log('opened:', device)
+    } catch (error) {
+      console.log(error)
+    }
+	  await device.close()
+  })
+})
